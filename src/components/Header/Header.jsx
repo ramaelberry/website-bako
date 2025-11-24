8ef024eb4bc08e1feda696306ad4f97e77d87271
ramaelberry <ramaelberry@aucegypt.edu>
2025-11-24 14:39:22 +0200

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your Favourite food here</h2>
+        <p>Delicious food</p> 
+        <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
